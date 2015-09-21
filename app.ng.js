904c57efe22eb1c48bc815d671cf99394055cab8
cisnb125 <cisnb125@gmail.com>
2015-09-22 00:21:04 +0900

    Initial commit

---
+++ app.ng.js
@@ -0,0 +1,30 @@
+// Will run on boh on the client and the server (mongo & minimongo)
+Parties = new Mongo.Collections("parties");
+
+// Everything inside this if statement will
+// only run on the client side.
+if (Meteor.isClient) {
+  angular.module('socially', ['angular-meteor']);
+
+  angular.module('socially').controller('PartiesListCtrl',
+    function($scope, $meteor) {
+      $scope.parties = $meteor.collection(Parties);
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function() {
+    if (Parites.find().count() === 0) {
+      var parties = [
+        {'name': 'Dubstep-Free Zone',
+          'description': 'Fast just got faster with Nexus S.'},
+        {'name': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'name': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+      for (var i = 0; i  < paries.length; i++)
+        Parties.insert(parties[i]);
+    }
+  });
+}
