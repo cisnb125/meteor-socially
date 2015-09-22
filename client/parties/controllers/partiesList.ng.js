e03fe789b826e585c6505a7d7080a58bebf9dd3a
cisnb125 <cisnb125@gmail.com>
2015-09-22 21:00:06 +0900

    Meteor methods

--- client/parties/controllers/partiesList.ng.js
+++ client/parties/controllers/partiesList.ng.js
@@ -1,10 +1,12 @@
-angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
-  function($scope, $meteor){
+angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor', '$rootScope',
+  function($scope, $meteor, $rootScope){
 
     $scope.page = 1;
     $scope.perPage = 3;
     $scope.sort = { name: 1 };
     $scope.orderProperty = '1';
+
+    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
     $scope.parties = $meteor.collection(function() {
       return Parties.find({}, {
@@ -38,4 +40,42 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
       if ($scope.orderProperty)
         $scope.sort = {name: parseInt($scope.orderProperty)};
     });
+
+    $scope.rsvp = function(partyId, rsvp) {
+      $meteor.call('rsvp', partyId, rsvp).then(
+        function(data) {
+          console.log('success responding', data);
+        },
+        function(err) {
+          console.log('failed', err);
+        }
+      );
+    };
+
+    $scope.outstandingInvitations = function (party) {
+      return _.filter($scope.users, function (user) {
+        return (_.contains(party.invited, user._id) &&
+        !_.findWhere(party.rsvps, {user: user._id}));
+      });
+    };
+
+    $scope.getUserById = function(userId) {
+      return Meteor.users.findOne(userId);
+    };
+
+    $scope.creator = function(party) {
+      if (!party)
+        return;
+
+      var owner = $scope.getUserById(party.owner);
+      if (!owner)
+        return 'nobody';
+
+      if ($rootScope.currentUser)
+        if ($rootScope.currentUser._id)
+          if (owner._id === $rootScope.currentUser._id)
+            return 'me';
+
+      return owner;
+    };
   }]);
