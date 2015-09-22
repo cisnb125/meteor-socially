8e1c3428145bb1c234851184b391e275a2194cbd
cisnb125 <cisnb125@gmail.com>
2015-09-22 15:23:33 +0900

    Set up routing

--- app.ng.js
+++ app.ng.js
@@ -4,13 +4,43 @@ Parties = new Mongo.Collection("parties");
 // Everything inside this if statement will
 // only run on the client side.
 if (Meteor.isClient) {
-  var app = angular.module('socially', ['angular-meteor']);
+  var app = angular.module('socially', ['angular-meteor', 'ui.router']);
+
+  app.config(function($urlRouterProvider, $stateProvider, $locationProvider) {
+    // Sets the URL to look like a regular one
+    $locationProvider.html5Mode(true);
+
+    $stateProvider
+      .state('parties', {
+        url: '/parties',
+        templateUrl: 'parties-list.ng.html',
+        controller: 'PartiesListCtrl'
+      })
+      .state('partyDetails', {
+        url: '/parties/:partyId',
+        templateUrl: 'party-details.ng.html',
+        controller: 'PartyDetailsCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/parties');
+  });
 
   app.controller('PartiesListCtrl',
     function($scope, $meteor) {
       // Bind parties to the Parties Mongo collection
       $scope.parties = $meteor.collection(Parties);
-      $scope.test = 'test';
+
+      $scope.remove = function(party) {
+        $scope.parties.remove(party);
+      };
+
+      $scope.removeAll = function() {
+        $scope.parties.remove();
+      }
+  });
+
+  app.controller('PartyDetailsCtrl', function($scope, $stateParams) {
+    $scope.partyId = $stateParams.partyId;
   });
 }
 
